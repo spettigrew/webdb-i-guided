32adb14a0fb78375534b4dc0ebb8b6e9edd42861
spettigrew <sara.pettigrew5@gmail.com>
2019-12-17 19:49:18 -0700

    Router requests inserted

--- posts/post-router.js
+++ posts/post-router.js
@@ -3,24 +3,60 @@ const db = require("../data/db-config") // database access using knex
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    try {
+       res.json(await db.select('*').from('posts'))
+    } catch (err) {
+        next
+    }
 })
 
-router.get("/:id", (req, res, next) => {
-
+router.get("/:id", async (req, res, next) => {
+    try {
+        // translates to 'SELECT * FROM posts WHERE id = ?' first = limit 1.
+        const post = await db('posts').where('id', req.params.id).first()//.select()
+        res.json(post)
+    } catch (err) {
+        next
+    }
 })
 
-router.post("/", (req, res, next) => {
-
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }   
+            // translate to 'INSERT INTO post (title, contents) VALUES(?, ?);'
+            const [id] = await db('posts').insert(payload)
+            return res.json(await db('posts').where('id', id).first())
+    } catch (err) {
+        next
+    }
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }   
 
+        await db('posts').where('id', req.params.id).update(payload)
+        return res.json(await db('posts').where('id', req.params.id).first())
+    } catch (err) {
+        next
+    }
 })
 
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db('posts').where('id', req.params.id).del()
+        // .end() sends an empty response
+        return res.status(204).end()
+    } catch (err) {
+        next
+    }
 })
 
 module.exports = router
